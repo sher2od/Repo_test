42cde5b8f5c5f06261ff3e66c7828f11732b9183
sher2od <sherzodqarshiboyev543@gmail.com>
2025-05-24 11:08:33 +0300

    m6

--- lesson6.py
+++ lesson6.py
@@ -5,9 +5,9 @@ summa = raund((oy_o - oy_b) * 0.45,2)
 dollars = int(summa)
 cents = int(round((summa - dollars) * 100))
 
-# So‘zga o‘girish (ingliz va rus)
+
 summa_en = f"{num2words(dollars, lang='en')} dollars and {num2words(cents, lang='en')} cents"
 summa_ru = f"{num2words(dollars, lang='ru')} долларов {num2words(cents, lang='ru')} центa"
 
-# Natijani chiqarish
+
 print(f"Tolov: ${summa:.2f} ({summa_en}, {summa_ru})")
