139349ce1f44a91823a51f021eb344140d95a6fe
sher2od <sherzodqarshiboyev543@gmail.com>
2025-05-24 10:16:43 +0300

    m3

--- lesson3.py
+++ lesson3.py
@@ -4,10 +4,15 @@ mahsulot_q = input("haftalik mahsulotni kiriting vergul bilan  >> ")
 bosh = 0
 for i in mahsulot_q.split(","):
     bosh += float(i)
-j = bosh / 7
+summa = bosh / 7
 
-print(f"ortacha xarajat {j:.2f}")
-print(num2words(j,lang="en",to = 'currency'))
-print(num2words(j,lang="ru",to = 'currency'))
+dollar = int(summa)
+cents = int(round((summa - dollars) * 100))
+
+
+summa_en = f"{num2words(dollars, lang='en')} dollars and {num2words(cents, lang='en')} cents"
+summa_ru = f"{num2words(dollars, lang='ru')} долларов {num2words(cents, lang='ru')} центa"
+
+print(f"Yetkazib berish narxi: ${summa:.2f} ({summa_en}, {summa_ru})")
 
 
