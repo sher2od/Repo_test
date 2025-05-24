9c12cdb88b60a82704af226763941d136c78d099
sher2od <sherzodqarshiboyev543@gmail.com>
2025-05-24 10:02:38 +0300

    m5

--- lesson5.py
+++ lesson5.py
@@ -1,18 +1,15 @@
 from num2words import num2words
 
-# Foydalanuvchidan masofa olish
-m = float(input("Masofani kiriting (Boshlang‘ich to‘lov $5.00) >> "))
 
-# Narxni hisoblash
-summa = round(5.00 + (m * 0.80), 2)  # ikkita kasr raqamga yaxlitladik
+m = float(input("Masofani kiriting (Boshlangich tolov $5.00) >> "))
 
-# Dollar va centlarga ajratish
+summa = round(5.00 + (m * 0.80), 2)  
+
 dollars = int(summa)
 cents = int(round((summa - dollars) * 100))
 
-# So‘zda ifoda (Inglizcha va Ruscha)
-summa_en = f"{num2words(dollars, lang='en')} dollars and {num2words(cents, lang='en')} cents"
-summa_ru = f"{num2words(dollars, lang='ru')} долларов {num2words(cents, lang='ru')} центов"
 
-# Natijani chiqarish
+summa_en = f"{num2words(dollars, lang='en')} dollars and {num2words(cents, lang='en')} cents"
+summa_ru = f"{num2words(dollars, lang='ru')} долларов {num2words(cents, lang='ru')} центa"
+
 print(f"Yetkazib berish narxi: ${summa:.2f} ({summa_en}, {summa_ru})")
